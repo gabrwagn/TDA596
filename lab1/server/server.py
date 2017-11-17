6990fde3a77db02670ed03e7d20ec51911ee8613
nordm065 <nordm065@umn.edu>
2017-11-17 18:48:02 +0100

    debugging...

--- lab1/server/server.py
+++ lab1/server/server.py
@@ -331,7 +331,7 @@ if __name__ == '__main__':
     ## read the templates from the corresponding html files
     # .....
     # Open the html files
-    folder = os.path.join(os.getcwd(), "")
+    folder = os.path.join(os.getcwd(), "server", "")
     header_fo = list(open(folder + board_frontpage_header_template, 'r'))
     body_fo = list(open(folder + boardcontents_template, 'r'))
     footer_fo = list(open(folder + board_frontpage_footer_template, 'r'))
