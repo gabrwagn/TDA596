bd6222806f25f8a561109d15b718d3372320ca62
None <None>
2017-11-23 04:37:29 -0800

    added new testing scripts

--- lab1/testingScripts/concurrentmodify.sh
+++ lab1/testingScripts/concurrentmodify.sh
@@ -1,2 +1,6 @@
-curl --data "entry=hello" http://10.1.0.1/board
-curl --data "entry=hello modified by 2&delete=0" http://10.1.0.2/board/0 & curl --data "entry=hello modified by 3&delete=0" http://10.1.0.3/board/0
+max=10
+for i in `seq 2 $max`
+do
+    curl --data "entry=hello" http://10.1.0.1/board
+    curl --data "entry=hello modified by 2&delete=0" http://10.1.0.2/board/0 & curl --data "entry=hello modified by 3&delete=0" http://10.1.0.3/board/0 & curl --data "entry=hello modified by 3&delete=0" http://10.1.0.3/board/0
+done
--- lab1/testingScripts/concurrentposts.sh
+++ lab1/testingScripts/concurrentposts.sh
@@ -1,1 +1,5 @@
-curl --data "entry=hello" http://10.1.0.1/board & curl --data "entry=bye" http://10.1.0.2/board & curl --data "entry=logging on from board 10" http://10.1.0.10/board
+max=10
+for i in `seq 2 $max`
+do
+    curl --data "entry=hello $i" http://10.1.0.1/board & curl --data "entry=bye" http://10.1.0.2/board & curl --data "entry=logging on from board 10" http://10.1.0.10/board
+done
