b73ba68a635c4646ddd5b85ffa115cb35ac165df
nordm065 <nordm065@umn.edu>
2017-12-07 17:55:28 +0100

    debug

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -86,7 +86,7 @@ class Lab1():
 	def run(self):
 		"Run the lab 1 simulation environment"
 		# local variables
-		nbOfServersPerRegion = 2
+		nbOfServersPerRegion = 3
 		nbOfClientsPerRegion = 2
 		nbOfRegions = 2
 		localJitter = 10 # ms, the evolution of the time between two consecutive packets
