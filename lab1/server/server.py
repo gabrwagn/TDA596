b4306db30866b06ae91805a046c797bd455acf5a
nordm065 <nordm065@umn.edu>
2017-12-07 16:01:01 +0100

    debug

--- lab1/server/server.py
+++ lab1/server/server.py
@@ -271,7 +271,7 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
                 if len(path_parts) > 1:
                     # A post containing an ID (delete/modify)
                     path_info = path_parts[1:]
-                    path_info[2] = str(int(path_info[2]) + 1)
+                    #path_info[2] = str(int(path_info[2]) + 1)
                     self.handle_entry(data, path_info)
                 else:
                     self.handle_entry(data)
