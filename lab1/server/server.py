af39ed59ad227d2bde84a035973a120530c81831
nordm065 <nordm065@umn.edu>
2017-12-23 20:08:07 +0100

    tabs

--- lab1/server/server.py
+++ lab1/server/server.py
@@ -31,70 +31,70 @@ PORT_NUMBER = 80
 #------------------------------------------------------------------------------------------------------
 class BlackboardServer(HTTPServer):
 #------------------------------------------------------------------------------------------------------
-	def __init__(self, server_address, handler, node_id, vessel_list):
-	# We call the super init
-		HTTPServer.__init__(self,server_address, handler)
-		# we create the dictionary of values
-		self.vote_one_store = {}
-		self.vote_two_store = []
-		# our own ID (IP is 10.1.0.ID)
-		self.vessel_id = vessel_id
-		# The list of other vessels
-		self.vessels = vessel_list
-		self.is_byzantine = False
+    def __init__(self, server_address, handler, node_id, vessel_list):
+    # We call the super init
+        HTTPServer.__init__(self,server_address, handler)
+        # we create the dictionary of values
+        self.vote_one_store = {}
+        self.vote_two_store = []
+        # our own ID (IP is 10.1.0.ID)
+        self.vessel_id = vessel_id
+        # The list of other vessels
+        self.vessels = vessel_list
+        self.is_byzantine = False
 #------------------------------------------------------------------------------------------------------
-	# We add a value received to the vote one store
-	def add_vote(self, sender, vote):
-		# We add the value to the store
-		pass
+    # We add a value received to the vote one store
+    def add_vote(self, sender, vote):
+        # We add the value to the store
+        pass
 
-	# We add a value received to the vote two store
-	def add_result_vector(self, vector):
-		# We add the value to the store
-		pass
+    # We add a value received to the vote two store
+    def add_result_vector(self, vector):
+        # We add the value to the store
+        pass
 #------------------------------------------------------------------------------------------------------
 # Contact a specific vessel with a set of variables to transmit to it
-	def contact_vessel(self, vessel_ip, path, action, key, value):
-		# the Boolean variable we will return
-		success = False
-		# The variables must be encoded in the URL format, through urllib.urlencode
-		post_content = urlencode({'action': action, 'key': key, 'value': value})
-		# the HTTP header must contain the type of data we are transmitting, here URL encoded
-		headers = {"Content-type": "application/x-www-form-urlencoded"}
-		# We should try to catch errors when contacting the vessel
-		try:
-			# We contact vessel:PORT_NUMBER since we all use the same port
-			# We can set a timeout, after which the connection fails if nothing happened
-			connection = HTTPConnection("%s:%d" % (vessel, PORT_NUMBER), timeout = 30)
-			# We only use POST to send data (PUT and DELETE not supported)
-			action_type = "POST"
-			# We send the HTTP request
-			connection.request(action_type, path, post_content, headers)
-			# We retrieve the response
-			response = connection.getresponse()
-			# We want to check the status, the body should be empty
-			status = response.status
-			# If we receive a HTTP 200 - OK
-			if status == 200:
-				success = True
-		# We catch every possible exceptions
-		except Exception as e:
-			print "Error while contacting %s" % vessel
-			# printing the error given by Python
-			print(e)
+    def contact_vessel(self, vessel_ip, path, action, key, value):
+        # the Boolean variable we will return
+        success = False
+        # The variables must be encoded in the URL format, through urllib.urlencode
+        post_content = urlencode({'action': action, 'key': key, 'value': value})
+        # the HTTP header must contain the type of data we are transmitting, here URL encoded
+        headers = {"Content-type": "application/x-www-form-urlencoded"}
+        # We should try to catch errors when contacting the vessel
+        try:
+            # We contact vessel:PORT_NUMBER since we all use the same port
+            # We can set a timeout, after which the connection fails if nothing happened
+            connection = HTTPConnection("%s:%d" % (vessel, PORT_NUMBER), timeout = 30)
+            # We only use POST to send data (PUT and DELETE not supported)
+            action_type = "POST"
+            # We send the HTTP request
+            connection.request(action_type, path, post_content, headers)
+            # We retrieve the response
+            response = connection.getresponse()
+            # We want to check the status, the body should be empty
+            status = response.status
+            # If we receive a HTTP 200 - OK
+            if status == 200:
+                success = True
+        # We catch every possible exceptions
+        except Exception as e:
+            print "Error while contacting %s" % vessel
+            # printing the error given by Python
+            print(e)
 
-		# we return if we succeeded or not
-		return success
+        # we return if we succeeded or not
+        return success
 #------------------------------------------------------------------------------------------------------
-	# We send a received value to all the other vessels of the system
-	def propagate_value_to_vessels(self, path, action, key, value):
-		# We iterate through the vessel list
-		for vessel in self.vessels:
-			# We should not send it to our own IP, or we would create an infinite loop of updates
-			if vessel != ("10.1.0.%s" % self.vessel_id):
-				# A good practice would be to try again if the request failed
-				# Here, we do it only once
-				self.contact_vessel(vessel, path, action, key, value)
+    # We send a received value to all the other vessels of the system
+    def propagate_value_to_vessels(self, path, action, key, value):
+        # We iterate through the vessel list
+        for vessel in self.vessels:
+            # We should not send it to our own IP, or we would create an infinite loop of updates
+            if vessel != ("10.1.0.%s" % self.vessel_id):
+                # A good practice would be to try again if the request failed
+                # Here, we do it only once
+                self.contact_vessel(vessel, path, action, key, value)
 #------------------------------------------------------------------------------------------------------
 
 
@@ -111,93 +111,93 @@ class BlackboardServer(HTTPServer):
 # Attributes of the server are SHARED accross all request hqndling/ threads!
 class BlackboardRequestHandler(BaseHTTPRequestHandler):
 #------------------------------------------------------------------------------------------------------
-	# We fill the HTTP headers
-	def set_HTTP_headers(self, status_code = 200):
-		 # We set the response status code (200 if OK, something else otherwise)
-		self.send_response(status_code)
-		# We set the content type to HTML
-		self.send_header("Content-type","text/html")
-		# No more important headers, we can close them
-		self.end_headers()
+    # We fill the HTTP headers
+    def set_HTTP_headers(self, status_code = 200):
+         # We set the response status code (200 if OK, something else otherwise)
+        self.send_response(status_code)
+        # We set the content type to HTML
+        self.send_header("Content-type","text/html")
+        # No more important headers, we can close them
+        self.end_headers()
 #------------------------------------------------------------------------------------------------------
-	# a POST request must be parsed through urlparse.parse_QS, since the content is URL encoded
-	def parse_POST_request(self):
-		post_data = ""
-		# We need to parse the response, so we must know the length of the content
-		length = int(self.headers['Content-Length'])
-		# we can now parse the content using parse_qs
-		post_data = parse_qs(self.rfile.read(length), keep_blank_values=1)
-		# we return the data
-		return post_data
+    # a POST request must be parsed through urlparse.parse_QS, since the content is URL encoded
+    def parse_POST_request(self):
+        post_data = ""
+        # We need to parse the response, so we must know the length of the content
+        length = int(self.headers['Content-Length'])
+        # we can now parse the content using parse_qs
+        post_data = parse_qs(self.rfile.read(length), keep_blank_values=1)
+        # we return the data
+        return post_data
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------
 # Request handling - GET
 #------------------------------------------------------------------------------------------------------
-	# This function contains the logic executed when this server receives a GET request
-	# This function is called AUTOMATICALLY upon reception and is executed as a thread!
-	def do_GET(self):
-		print("Receiving a GET on path %s" % self.path)
-		# Here, we should check which path was requested and call the right logic based on it
-		self.do_GET_Index()
+    # This function contains the logic executed when this server receives a GET request
+    # This function is called AUTOMATICALLY upon reception and is executed as a thread!
+    def do_GET(self):
+        print("Receiving a GET on path %s" % self.path)
+        # Here, we should check which path was requested and call the right logic based on it
+        self.do_GET_Index()
 #------------------------------------------------------------------------------------------------------
 # GET logic - specific path
 #------------------------------------------------------------------------------------------------------
-	def do_GET_Index(self):
-		# We set the response status code to 200 (OK)
-		self.set_HTTP_headers(200)
-		# We should do some real HTML here
-		html_reponse = frontpage_template_fo
-		#In practice, go over the entries list,
-		#produce the boardcontents part,
-		#then construct the full page by combining all the parts ...
+    def do_GET_Index(self):
+        # We set the response status code to 200 (OK)
+        self.set_HTTP_headers(200)
+        # We should do some real HTML here
+        html_reponse = frontpage_template_fo
+        #In practice, go over the entries list,
+        #produce the boardcontents part,
+        #then construct the full page by combining all the parts ...
 
-		self.wfile.write(html_reponse)
+        self.wfile.write(html_reponse)
 #------------------------------------------------------------------------------------------------------
-	# we might want some other functions
+    # we might want some other functions
 #------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------
 # Request handling - POST
 #------------------------------------------------------------------------------------------------------
-	def do_POST(self):
-		print("Receiving a POST on %s" % self.path)
-		# Here, we should check which path was requested and call the right logic based on it
-		# We should also parse the data received
-		# and set the headers for the client
+    def do_POST(self):
+        print("Receiving a POST on %s" % self.path)
+        # Here, we should check which path was requested and call the right logic based on it
+        # We should also parse the data received
+        # and set the headers for the client
 
-		data = self.parse_POST_request()
-		print data
+        data = self.parse_POST_request()
+        print data
         path_parts = self.path[1:].split('/')
         sender = data["sender"][0] # Don't know if this is str or int
         
-		if path_parts[1] == "attack":
-			# Handle attack logic
-			self.server.add_vote(sender, "attack")
-		elif path_parts[1] == "retreat":
-			# Handle retreat logic
-			self.server.add_vote(sender, "retreat")
-		elif path_parts[1] == "byzantine":
-			# Call their fuctions
-			self.server.is_byzantine = True
+        if path_parts[1] == "attack":
+            # Handle attack logic
+            self.server.add_vote(sender, "attack")
+        elif path_parts[1] == "retreat":
+            # Handle retreat logic
+            self.server.add_vote(sender, "retreat")
+        elif path_parts[1] == "byzantine":
+            # Call their fuctions
+            self.server.is_byzantine = True
 
-		# If we want to retransmit what we received to the other vessels
-		retransmit = True
-		if sender != self.server.vessel_id:
-			retransmit = False
-		 # Like this, we will just create infinite loops!
-		if retransmit:
-			# do_POST send the message only when the function finishes
-			# We must then create threads if we want to do some heavy computation
-			#
-			# Random content
-			thread = Thread(target=self.server.propagate_value_to_vessels,args=("action", "key", "value") )
-			# We kill the process if we kill the server
-			thread.daemon = True
-			# We start the thread
-			thread.start()
+        # If we want to retransmit what we received to the other vessels
+        retransmit = True
+        if sender != self.server.vessel_id:
+            retransmit = False
+         # Like this, we will just create infinite loops!
+        if retransmit:
+            # do_POST send the message only when the function finishes
+            # We must then create threads if we want to do some heavy computation
+            #
+            # Random content
+            thread = Thread(target=self.server.propagate_value_to_vessels,args=("action", "key", "value") )
+            # We kill the process if we kill the server
+            thread.daemon = True
+            # We start the thread
+            thread.start()
 #------------------------------------------------------------------------------------------------------
 # POST Logic
 #------------------------------------------------------------------------------------------------------
-	# We might want some functions here as well
+    # We might want some functions here as well
 #------------------------------------------------------------------------------------------------------
 
 
@@ -209,31 +209,31 @@ class BlackboardRequestHandler(BaseHTTPRequestHandler):
 # Execute the code
 if __name__ == '__main__':
 
-	## read the templates from the corresponding html files
-	# .....
+    ## read the templates from the corresponding html files
+    # .....
 
-	vessel_list = []
-	vessel_id = 0
-	# Checking the arguments
-	if len(sys.argv) != 3: # 2 args, the script and the vessel name
-		print("Arguments: vessel_ID number_of_vessels")
-	else:
-		# We need to know the vessel IP
-		vessel_id = int(sys.argv[1])
-		# We need to write the other vessels IP, based on the knowledge of their number
-		for i in range(1, int(sys.argv[2])+1):
-			vessel_list.append("10.1.0.%d" % i) # We can add ourselves, we have a test in the propagation
+    vessel_list = []
+    vessel_id = 0
+    # Checking the arguments
+    if len(sys.argv) != 3: # 2 args, the script and the vessel name
+        print("Arguments: vessel_ID number_of_vessels")
+    else:
+        # We need to know the vessel IP
+        vessel_id = int(sys.argv[1])
+        # We need to write the other vessels IP, based on the knowledge of their number
+        for i in range(1, int(sys.argv[2])+1):
+            vessel_list.append("10.1.0.%d" % i) # We can add ourselves, we have a test in the propagation
 
 
-	folder = os.path.join(os.getcwd(), "server", "vote_frontpage_template.html")
-	frontpage_template_fo = list(open(folder, 'r'))
-	# We launch a server
-	server = BlackboardServer(('', PORT_NUMBER), BlackboardRequestHandler, vessel_id, vessel_list)
-	print("Starting the server on port %d" % PORT_NUMBER)
+    folder = os.path.join(os.getcwd(), "server", "vote_frontpage_template.html")
+    frontpage_template_fo = list(open(folder, 'r'))
+    # We launch a server
+    server = BlackboardServer(('', PORT_NUMBER), BlackboardRequestHandler, vessel_id, vessel_list)
+    print("Starting the server on port %d" % PORT_NUMBER)
 
-	try:
-		server.serve_forever()
-	except KeyboardInterrupt:
-		server.server_close()
-		print("Stopping Server")
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
+        print("Stopping Server")
 #------------------------------------------------------------------------------------------------------
